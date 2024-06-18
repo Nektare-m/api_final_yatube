9750d031755ce4ad216a1792cf3971091ac10520
Nektare <nektare-m@gmail.com>
2024-06-18 13:38:07 +1000

    hw

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,4 +1,3 @@
-from django.core.exceptions import PermissionDenied
 from django.contrib.auth import get_user_model
 from django.shortcuts import get_object_or_404
 from rest_framework import filters, permissions, status, viewsets
